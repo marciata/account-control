001e79036073c03643b98c2b8066c720e6b05897
marciata <applications.bitwos@gmail.com>
2024-01-22 00:10:05 -0300

    versao inicial

---
+++ src/main/java/com/canal/curso/de/tecnologia/accountcontrol/AccountControlApplication.java
@@ -0,0 +1,13 @@
+package com.canal.curso.de.tecnologia.accountcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/canal/curso/de/tecnologia/accountcontrol/AccountControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canal.curso.de.tecnologia.accountcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
